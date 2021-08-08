62203aed3ffe0a835be1936e82b48f260b404aef
Kim Carpico <kcarpico@gmail.com>
2021-08-08 16:39:28 -0700

    cont js zero to hero course

--- index.js
+++ index.js
@@ -564,18 +564,60 @@
 //   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 // };
 
-// ES6 - arrow function version
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// // ES6 - arrow function version
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
 
-// BONUSSSSS
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(bills, tips, totals);
+// // BONUSSSSS
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills, tips, totals);
 
 //////////////////////////////////////
 // INTRODUCTION TO OBJECTS
 //////////////////////////////////////
+// // array
+// const jonasArray = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
+
+// // object with key-value pairs, prop in objects return alphabetically but we access them with dot or bracket notation
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+
+// console.log(jonas);
+
+// // DOT VS. BRACKET NOTATION
+// // dot notation
+// console.log(jonas.lastName);
+
+// // bracket notation
+// console.log(jonas["lastName"]);
+
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// const interestedIn = window.prompt(
+//   "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends."
+// );
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request! Choose between firstName, lastName, age, job, and friends."
+//   );
+// }
